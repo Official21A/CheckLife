2ab13867006532e2072d0e62ceb63c0f1335efbb
Official21A <officialamirhossein21@gmail.com>
2020-10-14 08:05:52 +0330

    Response

--- script.py
+++ script.py
@@ -6,6 +6,8 @@ TIMEOUT = 1
 def open_connection(url):
 	try:
 		response = requests.get(url, timeout=TIMEOUT, allow_redirects=False)
+		print(f":> Result: URL={response.url} >> ", end="")
+		print(f"{response.status_code} {response.reason}")
 	except:
-		print("::> Program failed, Something is wrong with your system.")	
+		print(":?Program failed, Something is wrong with your system.")	
 		sys.exit(-1)
