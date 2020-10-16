f8d0995b74001b364a3d0c23c24a12b599ce0c4c
Official21A <officialamirhossein21@gmail.com>
2020-10-16 09:36:40 +0330

    Consol done

--- script.py
+++ script.py
@@ -3,10 +3,40 @@ import sys
 import threading
 import time
 
+PATH = "DATA"
 TIMEOUT = 1
 
 results = {}
 url_list = []
+
+
+def save(value):
+	try:
+		with open(PATH, "a+") as file:
+			file.write(f"{value}\n")
+			file.close()
+	except (e,):
+		print(e)	
+
+
+def load():
+	global url_list
+	try:
+		with open(PATH, "r") as file:
+			url_list = file.readlines()
+	except (e,):
+		print(e)						
+
+
+def update(value):
+	global url_list
+	try:
+		with open(PATH, "w") as file:
+			for line in url_list:
+				if line.strip() != value:
+					file.write(f"{line}")						
+	except (e,):
+		print(e)
 
 
 def open_connection(url):
@@ -28,7 +58,7 @@ def start_a_check(id, url):
 def execute(url_list):
 	id = 127
 	for url in url_list:
-		t = threading.Thread(target=start_a_check, args=(id, url))
+		t = threading.Thread(target=start_a_check, args=(id, url.strip()))
 		t.start()
 		id += 12
 
@@ -41,7 +71,7 @@ def print_all():
 
 def create():
 	global url_list
-	url_list = ["https://github.com","www.google.com","https://www.upgrad.com"]
+	load()
 	execute(url_list)
 	while True:
 		if len(results.keys()) == len(url_list):
@@ -57,11 +87,13 @@ def show():
 def add(value):
 	url_list.append(value)	
 	print(f">{value} : Added.")
+	save(value)
 
 
 def remove(value):
-	global url_list
-	print(f">{url_list.pop(value)} : Removed")
+	load()
+	update(value)
+	print(f">{value} : Removed")
 
 
 def run(arvg):
@@ -78,6 +110,7 @@ def run(arvg):
 			print(">? Error")
 	except (e,):
 		print(e)						
+
 
 if __name__ == "__main__":
 	run(sys.argv)		
